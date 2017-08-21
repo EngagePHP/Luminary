350c49ae6280ac51d46abe202377387bb8d23bca
Matthew Cuyar <matt@enctypeapparel.com>
2017-08-21 15:25:57 -0400

    formatting fixes

--- luminary/Services/Tenants/Exceptions/TenantNotSetException.php
+++ luminary/Services/Tenants/Exceptions/TenantNotSetException.php
@@ -4,4 +4,7 @@ namespace Luminary\Services\Tenants\Exceptions;
 
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class TenantNotSetException extends NotFoundHttpException {}
+class TenantNotSetException extends NotFoundHttpException
+{
+
+}
--- luminary/Services/Testing/database/factories/factory.php
+++ luminary/Services/Testing/database/factories/factory.php
@@ -4,26 +4,26 @@ use Luminary\Services\Testing\Models\Customer;
 use Luminary\Services\Testing\Models\Location;
 use Luminary\Services\Testing\Models\User;
 
-$tenant_id = function() {
+$tenant_id = function () {
     static $first;
     static $second;
 
     // make sure we have at least one
-    if(is_null($first)) {
+    if (is_null($first)) {
         $first = 1234;
         return 1234;
     }
 
     // make sure we have at least one
-    if(is_null($second)) {
+    if (is_null($second)) {
         $second = 2345;
         return 2345;
     }
 
-    return rand(0,1) ? 1234 : 2345;
+    return rand(0, 1) ? 1234 : 2345;
 };
 
-$factory->define(User::class, function (Faker\Generator $faker) use($tenant_id) {
+$factory->define(User::class, function (Faker\Generator $faker) use ($tenant_id) {
     return [
         'first_name' => $faker->firstName,
         'last_name' => $faker->lastName,
@@ -32,7 +32,7 @@ $factory->define(User::class, function (Faker\Generator $faker) use($tenant_id) 
     ];
 });
 
-$factory->define(Customer::class, function (Faker\Generator $faker) use($tenant_id) {
+$factory->define(Customer::class, function (Faker\Generator $faker) use ($tenant_id) {
     return [
         'name' => $faker->company,
         'website' => $faker->domainName,
@@ -41,7 +41,7 @@ $factory->define(Customer::class, function (Faker\Generator $faker) use($tenant_
     ];
 });
 
-$factory->define(Location::class, function (Faker\Generator $faker) use($tenant_id) {
+$factory->define(Location::class, function (Faker\Generator $faker) use ($tenant_id) {
     return [
         'name' => $faker->company,
         'street' => $faker->streetAddress,
