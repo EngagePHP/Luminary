aceced920ccf2205754ab93da0a5c65f48d190a4
Matthew Cuyar <matt@enctypeapparel.com>
2018-04-03 09:13:40 -0400

    up[dated policy generator and scaffold

--- luminary/Policies/PolicyRegistrar.php
+++ luminary/Policies/PolicyRegistrar.php
@@ -2,8 +2,18 @@
 
 namespace Luminary\Policies;
 
-class PolicyRegistrar
+use Luminary\Database\Eloquent\Model;
+
+abstract class PolicyRegistrar
 {
+    /**
+     * The Policies permissions
+     * to sync with the database
+     *
+     * @var array
+     */
+    public $permissions = [];
+
     /**
      * The Policies to register
      *
@@ -19,5 +29,33 @@ class PolicyRegistrar
     public function policies() :array
     {
         return $this->policies;
+    }
+
+    /**
+     * Get the permissions array
+     *
+     * @return array
+     */
+    public function permissions() :array
+    {
+        return $this->permissions;
+    }
+
+    /**
+     * Get a policy registrar
+     * model instance
+     *
+     * @return Model
+     */
+    abstract public function model();
+
+    /**
+     * Get the policy registrar model type
+     *
+     * @return string
+     */
+    public function type()
+    {
+        return $this->model()->getType();
     }
 }
