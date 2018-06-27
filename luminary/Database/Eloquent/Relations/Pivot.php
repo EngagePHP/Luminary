8c94c57b08c0d781c9396f9d76d47d51c260abc7
Matthew Cuyar <matt@enctypeapparel.com>
2018-06-27 19:42:28 -0400

    Updated the Luminary Pivot model

--- luminary/Database/Eloquent/Relations/Pivot.php
+++ luminary/Database/Eloquent/Relations/Pivot.php
@@ -8,4 +8,32 @@ use Luminary\Database\Eloquent\ModelTrait;
 class Pivot extends LumenPivot
 {
     use ModelTrait;
+
+    /**
+     * Get the name of the "updated at" column.
+     *
+     * @return string
+     */
+    public function getUpdatedAtColumn()
+    {
+        if ($this->pivotParent) {
+            return parent::getUpdatedAtColumn();
+        }
+
+        return static::UPDATED_AT;
+    }
+
+    /**
+     * Get the name of the "created at" column.
+     *
+     * @return string
+     */
+    public function getCreatedAtColumn()
+    {
+        if ($this->pivotParent) {
+            return parent::getCreatedAtColumn();
+        }
+
+        return static::CREATED_AT;
+    }
 }
