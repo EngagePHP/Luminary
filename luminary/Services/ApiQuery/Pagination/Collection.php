54e728885746aa1a2efce15af9c6ff6c9ca8543f
Matthew Cuyar <matt@enctypeapparel.com>
2018-11-14 15:16:40 -0500

    Added default paginator set

--- luminary/Services/ApiQuery/Pagination/Collection.php
+++ luminary/Services/ApiQuery/Pagination/Collection.php
@@ -4,9 +4,31 @@ namespace Luminary\Services\ApiQuery\Pagination;
 
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Pagination\AbstractPaginator;
+use Luminary\Services\ApiQuery\Pagination\Paginators\LengthAwarePaginator;
 
 class Collection extends EloquentCollection
 {
+    /**
+     * The paginator total
+     *
+     * @var int
+     */
+    public $total = 0;
+
+    /**
+     * The paginator per page
+     *
+     * @var int
+     */
+    public $perPage = 15;
+
+    /**
+     * The paginator current page
+     *
+     * @var int
+     */
+    public $currentPage = 1;
+
     /**
      * The paginator instance
      *
@@ -31,6 +53,13 @@ class Collection extends EloquentCollection
      */
     public function getPaginator() :AbstractPaginator
     {
+        $paginator = static::$paginator;
+
+        if(!$paginator) {
+            $this->setDefaultPaginator();
+            return $this->paginator();
+        }
+
         return static::$paginator;
     }
 
@@ -53,6 +82,25 @@ class Collection extends EloquentCollection
     public function setPaginator(AbstractPaginator $paginator) :Collection
     {
         static::$paginator = $paginator;
+
+        $this->total = $paginator->total();
+        $this->currentPage = $paginator->currentPage();
+        $this->perPage = $paginator->perPage();
+
+        return $this;
+    }
+
+    /**
+     * Set the paginator instance
+     *
+     * @return Collection
+     */
+    public function setDefaultPaginator() :Collection
+    {
+        $args = [$this->all(), $this->total, $this->perPage, $this->currentPage];
+        $paginator = new LengthAwarePaginator(...$args);
+
+        $this->setPaginator($paginator);
 
         return $this;
     }
