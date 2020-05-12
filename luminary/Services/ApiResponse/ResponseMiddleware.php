b568ac036b366b7c71df695097ccf60559cffb41
Matthew Cuyar <matt@enctypeapparel.com>
2020-05-12 14:53:08 -0400

    Added pagination response for related endpoints

--- luminary/Services/ApiResponse/ResponseMiddleware.php
+++ luminary/Services/ApiResponse/ResponseMiddleware.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
-use Luminary\Services\ApiQuery\Pagination\Collection as PaginationCollection;
 use Luminary\Services\ApiResponse\Serializers\ArraySerializer;
 use Luminary\Services\ApiResponse\Serializers\CollectionRelatedSerializer;
 use Luminary\Services\ApiResponse\Serializers\CollectionRelationshipSerializer;
